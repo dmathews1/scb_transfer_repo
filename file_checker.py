67119b7277295526cbb3d67468a0c2fffcfe985a
dmathews1 <72001017+dmathews1@users.noreply.github.com>
2021-11-16 11:14:41 +1100

    Add files via upload

--- file_checker.py
+++ file_checker.py
@@ -4,16 +4,28 @@ import os
 import argparse
 import re
 from sys import exit
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p','--path',dest='base_path', action='store', default = None)
+parser.add_argument('-f','--hdfs_path',dest='hdfs_path', action='store', default = None)
 parser.add_argument('-s','--source_system', dest='source_system', action='store', default = False)
 parser.add_argument('-c','--country', dest='country', action='store', default = False)
 parser.add_argument('-d','--database', dest='database', action='store', default = False)
 args = parser.parse_args()
 
-full_path = "{}/{}/{}/status".format(args.base_path, args.source_system, args.country)
-files = [ x for x in os.listdir(full_path) if re.search(r'\.*success*', x) ]
+base_command = "hdfs dfs -ls {}".format(args.hdfs_path)
+process = subprocess.Popen(base_command.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+hdfs_files = [ x.split()[-1] for x in output.split("\n")[1:-1]]
+
+##fail files
+check_fail_path = "hdfs dfs -ls {}/global/".format(args.hdfs_path)
+process_fail_files = subprocess.Popen(check_fail_path.split(), stdout=subprocess.PIPE)
+fail_output, fail_error = process_fail_files.communicate()
+fail_files = [ x.split()[-1] for x in fail_output.split("\n")[1:-1]]
+
+files = [ x for x in hdfs_files if re.search(r'\.*success*', x) ]
 
 if len(files) == 0:
     print("failure noted at a global level, not continuing")
@@ -24,8 +36,9 @@ dbs = [ x for x in args.database.split(",") if x ]
 ## grab tables
 
 table_path = "{}/{}/{}/configs".format(args.base_path, args.source_system, args.country)
-tableslist = [ x for x in os.listdir(table_path) if re.search(r'\.*{}*'.format(args.source_system), x) and re.search(r'\.*{}*'.format(args.country), x) and x.endswith(".xml") ]  
-tables = [ "_".join(x.split("_")[2:-2]) for x in tableslist ]
+tableslist = [ x for x in os.listdir(table_path) if re.search(r'\.*{}*'.format(args.source_system), x) and re.search(r'\.*{}*'.format(args.country), x) and x.endswith(".xml") ]
+tables = [ "_".join(x.split("_")[2:-2]) for x in tableslist]
+tables = [ x for x in tables if x ]
 
 if len(tables) < 0:
     print("no tables found")
@@ -33,15 +46,12 @@ if len(tables) < 0:
 
 ## check files
 
-check_fail_path = "{}/global/{}_{}_tokenization_failure_details".format(full_path, args.source_system, args.country)
-
-if os.path.exists(check_fail_path):
-    with open(check_fail_path) as f:
-        failed_tables_list = [ x for x in f.read().split("\n") if x ]
-    failed_tables = [ "_".join(x.split("_")[2:]) for x in failed_tables_list ]
-#    print(failed_tables)
-    if len(failed_tables) > 0:
-        tables = [ x for x in tables if x not in failed_tables ]
+if len(fail_files) == 1:
+    base_command = "hdfs dfs -cat {}/global/{}_{}_tokenization_failure_details".format(args.hdfs_path, args.source_system, args.country)
+    process = subprocess.Popen(base_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    fail_files = [ "_".join(x.split("_")[2:]) for x in output.split("\n") if x ]
+    tables = [ x for x in tables if x not in fail_files ]
 else:
     print("no failed tables")
 
@@ -51,4 +61,3 @@ for database in dbs:
 db_tables = [x for y in db_tables for x in y]
 
 print("\n".join(db_tables))
-

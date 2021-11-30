3974d7dd042dac6667f7748a982c2a20a023dedb
dmathews1 <72001017+dmathews1@users.noreply.github.com>
2021-11-30 17:56:55 +1100

    Add files via upload

--- file_checker.py
+++ file_checker.py
@@ -30,7 +30,7 @@ dbs = [ x for x in args.database.split(",") if x ]
 ## grab tables
 if args.tables == "":
     table_path = "{}/{}/{}/configs".format(args.base_path, args.source_system, args.country)
-    tableslist = [ x for x in os.listdir(table_path) if re.search(r'{}'.format(args.source_system), x) and re.search(r'{}'.format(args.country), x) and x.endswith(".xml") and not x.endswith("_param.xml") ]
+    tableslist = [ x for x in os.listdir(table_path) if re.search(r'{}'.format(args.source_system), x) and re.search(r'{}'.format(args.country), x) and x.endswith("_table_config.xml") ]
     tables = [ "_".join(x.split("_")[:-2]) for x in tableslist]
     tables = [ x for x in tables if x ]
 else:

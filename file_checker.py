041d90b15053d0166102d753dcc80ec574446ebd
dmathews1 <72001017+dmathews1@users.noreply.github.com>
2021-12-13 13:30:23 +1100

    Add files via upload

--- file_checker.py
+++ file_checker.py
@@ -14,17 +14,20 @@ parser.add_argument('-c','--country', dest='country', action='store', default = 
 parser.add_argument('-d','--database', dest='database', action='store', default = False)
 parser.add_argument('-x','--partition', dest='partition', action='store', default = False)
 parser.add_argument('-t','--tables', dest='tables', action='store', default = "")
+parser.add_argument('-m','--mapping', dest='mapping', action='store', default = "")
 parser.add_argument('-r','--replay', dest='replay', action='store_true', default = False)
 args = parser.parse_args()
 
-args.partition = args.partition.replace("-","_")
+partition = args.partition.replace("-","_")
+
+mapping = args.mapping.split(",")
 
 base_command = "/usr/hdp/current/hadoop-client/bin/hdfs dfs -ls {}/global/".format(args.hdfs_path)
 process = subprocess.Popen(base_command.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
 hdfs_files = [ x.split()[-1] for x in output.split("\n")[1:-1]]
 
-files = [ x for x in hdfs_files if re.search(r'_tokenization_success', x) and re.search(r'{}'.format(args.partition), x) ]
+files = [ x for x in hdfs_files if re.search(r'_tokenization_success', x) and re.search(r'{}'.format(partition), x) ]
 dbs = [ x for x in args.database.split(",") if x ]
 
 ## grab tables
@@ -41,7 +44,7 @@ if len(tables) < 0:
     exit(1)
 
 ## check files
-fail_files = [ x for x in hdfs_files if re.search(r'_tokenization_failure_details', x) and re.search(r'{}'.format(args.partition), x) ]
+fail_files = [ x for x in hdfs_files if re.search(r'_tokenization_failure_details', x) and re.search(r'{}'.format(partition), x) ]
 if len(fail_files) > 0:
     base_command = "/usr/hdp/current/hadoop-client/bin/hdfs dfs -cat {}".format(fail_files[0])
     process = subprocess.Popen(base_command.split(), stdout=subprocess.PIPE)
@@ -52,11 +55,19 @@ if len(fail_files) > 0:
 db_tables = []
 for database in dbs:
     db_tables.append([ "{}.{}".format(database.lower(), x) for x in tables ])
-db_tables = [x for y in db_tables for x in y]
-db_tables = [ "msck repair table {}".format(x) for x in db_tables if x not in fail_files ]
+db_tables = [x for y in db_tables for x in y if x not in fail_files ]
+
+if args.mapping is not "":
+    for i in db_tables:
+        index = db_tables.index(i)
+        for j in mapping:
+            if i.split(".")[0] == j.split(":")[0]:
+                db_tables[index] = (i, j.split(":")[1])
+    db_tables = [ "alter table {} add partition ({} = '{}');".format(x[0], x[1], args.partition) for x in db_tables ]
+else:
+    db_tables = [ "msck repair table {};".format(x) for x in db_tables ]
 
 if not args.replay:
     print("\n".join(db_tables))
 else:
     print("\n".join(fail_files))
-

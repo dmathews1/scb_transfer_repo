cef88bd03130e644d9c343613eefbb05b6ff6f61
dmathews1 <72001017+dmathews1@users.noreply.github.com>
2022-02-22 13:28:15 +1100

    Add files via upload

--- list_files.py
+++ list_files.py
@@ -14,6 +14,7 @@ parser.add_argument('-m','--mapping', dest='mapping', action='store', default = 
 parser.add_argument('-p','--partition', dest='partition', action='store', default = None)
 parser.add_argument('-u','--user_principal', dest='user_principal', action='store', default = None)
 parser.add_argument('-k','--keytab', dest='keytab', action='store', default = None)
+parser.add_argument('-f','--function', dest='function', action='store', default = None)
 args = parser.parse_args()
 
 os.environ["KRB5CCNAME"] = "FILE:/tmp/krb5cc_%{uid}"
@@ -21,10 +22,13 @@ base_command = "kinit -kt {} {}".format(args.keytab, args.user_principal)
 process = subprocess.Popen(base_command.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
 
+table_list = [ x[:-2] for x in args.tables.split(",") if x[-1] == args.function.strip() ]
 mapping = args.mapping.split(",")
-
 for db in args.databases.split(","):
     col_name = [ x.split(":")[1] for x in mapping if db in x ][0]
-    for tbl in args.tables.split(","):
-        directory_path = "hdfs dfs -ls {}/{}/{}/{}={} ".format(args.base_path, db, tbl, col_name, args.partition)
-        os.system("hdfs dfs -ls %s | awk '{print $8}' | awk '!/^$/' 2>/dev/null" % (directory_path))
+    for tbl in table_list:
+        hdfs_command = "hdfs dfs -ls {}/{}/{}/{}={}".format(args.base_path, db, tbl, col_name, args.partition)
+        hdfs_process = subprocess.Popen(hdfs_command.split(), stdout=subprocess.PIPE)
+        output, error = hdfs_process.communicate()
+        if output:
+            print("\n".join([x.split(" ")[-1] for x in output.split("\n") if x][1:]))
